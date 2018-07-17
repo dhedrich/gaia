ad17b11443ad88c49e51b911552d01e5e1a62978
dhedrich <chilidawg676@gmail.com>
2018-07-16 22:37:25 -0700

    style updates

--- public/history.js
+++ public/history.js
@@ -1,39 +1,72 @@
-$(document).ready(
+$(document).ready(function() {
   // grab all temp/rh data in db, render to homepage
   $.ajax({
-      method: 'GET',
-      url: '/history',
+    method: 'GET',
+    url: '/history',
+  })
+  .done(function (res) {
+    console.log('SUCCESS =================', res)
+    renderTable(res)
+  })
+  .fail(function (err) {
+    console.log('ERROR ===================', err)
+  })
+
+  $("#submit").on('click', function(event){
+    event.preventDefault()
+    var formData = {
+      email : $("#email").val().trim(),
+      tempHigh : parseInt($("#tempHigh").val().trim()),
+      tempLow : parseInt($("#tempLow").val().trim()),
+      rhHigh : parseInt($("#rhHigh").val().trim()),
+      rhLow : parseInt($("#rhLow").val().trim())
+    }
+    $.ajax({
+      method: 'POST',
+      url: '/user',
+      data: formData
     })
-    .done(function (res) {
-      console.log('SUCCESS =================', res)
-      renderTable(res)
+    .done(function (data) {
+      console.log('SUCCESS =================', data)
     })
     .fail(function (err) {
       console.log('ERROR ===================', err)
     })
-  )
+    $("#email").val('')
+    $("#tempHigh").val('')
+    $("#tempLow").val('')
+    $("#rhHigh").val('')
+    $("#rhLow").val('')
+  })
 
-function renderTable(res) {
-  // empty recents table
-  $('#recentsTable').empty().append(`<tr>
-      <th>Timestamp</th>
-      <th>Temperature</th>
-      <th>Humidity</th>
-    </tr>`)
+  function renderTable(res) {
+    // empty recents table
+    $('#recentsTable').empty().append(`<thead>
+          <tr class="table-primary">
+            <th id="table-left" scope="col"></th>  
+            <th scope="col">Timestamp</th>
+            <th scope="col">Temperature</th>
+            <th id="table-right" scope="col">Humidity</th>
+          </tr>
+        </thead>`)
 
-  // post data to table
-  for (var i = res.length - 1; i >= 0; i--) {
-    var timestamp = res[i].timestamp
-    var temp = res[i].temp
-    var rh = res[i].rh
-    $('#recentsTable').append(`<tr>
-      <td>${timestamp}</td>
-      <td>${temp} °C</td>
-      <td>${rh} %</td>
-    </tr>`)
+    // post data to table
+    var count = 1 
+    for (var i = res.length - 1; i >= 0; i--) {
+      var timestamp = res[i].timestamp
+      var temp = res[i].temp
+      var rh = res[i].rh
+      $('#recentsTable').append(`<tr>
+        <th scope="row">${count}</th>
+        <td>${timestamp}</td>
+        <td>${temp} °C</td>
+        <td>${rh} %</td>
+      </tr>`)
+      count++
+    }
+
+    // update current temp/rh
+    $('#currTemp').text(`${res[res.length -1].temp} °C`)
+    $('#currRH').text(`${res[res.length -1].rh} %`)
   }
-
-  // update current temp/rh
-  $('#currTemp').text(`${res[res.length -1].temp} °C`)
-  $('#currRH').text(`${res[res.length -1].rh} %`)
-}
+})
